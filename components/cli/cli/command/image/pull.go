422169b4d31c351d90f497c48aac2b6d9be36c3a
Sami Shahin <ssshahin@bu.edu>
2018-07-10 14:39:39 -0400

    Added --source flag to docker pull

--- components/cli/cli/command/image/pull.go
+++ components/cli/cli/command/image/pull.go
@@ -4,11 +4,17 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"runtime"
 
 	"github.com/docker/cli/cli"
 	"github.com/docker/cli/cli/command"
 	"github.com/docker/cli/cli/trust"
+	"github.com/docker/cli/cli/manifest/types"
+	"github.com/containerd/containerd/platforms"
+	"github.com/docker/distribution"
+	"github.com/docker/distribution/manifest/manifestlist"
 	"github.com/docker/distribution/reference"
+	"github.com/docker/docker/registry"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
@@ -19,6 +25,7 @@ type PullOptions struct {
 	all       bool
 	platform  string
 	untrusted bool
+	source bool
 }
 
 // NewPullCommand creates a new `docker pull` command
@@ -38,6 +45,7 @@ func NewPullCommand(dockerCli command.Cli) *cobra.Command {
 	flags := cmd.Flags()
 
 	flags.BoolVarP(&opts.all, "all-tags", "a", false, "Download all tagged images in the repository")
+	flags.BoolVar(&opts.source, "source", false, "Download source for specified image")
 
 	command.AddPlatformFlag(flags, &opts.platform)
 	command.AddTrustVerificationFlags(flags, &opts.untrusted, dockerCli.ContentTrustEnabled())
@@ -53,11 +61,18 @@ func RunPull(cli command.Cli, opts PullOptions) error {
 		return err
 	case opts.all && !reference.IsNameOnly(distributionRef):
 		return errors.New("tag can't be used with --all-tags/-a")
+	case opts.all && opts.source:
+		return errors.New("can't use --source with --all-tags/-a")
 	case !opts.all && reference.IsNameOnly(distributionRef):
 		distributionRef = reference.TagNameOnly(distributionRef)
 		if tagged, ok := distributionRef.(reference.Tagged); ok {
 			fmt.Fprintf(cli.Out(), "Using default tag: %s\n", tagged.Tag())
 		}
+	}
+
+	// Check if source flag passed
+	if opts.source {
+		return sourcePull(cli, distributionRef, opts)
 	}
 
 	ctx := context.Background()
@@ -80,4 +95,115 @@ func RunPull(cli command.Cli, opts PullOptions) error {
 		return err
 	}
 	return nil
+}
+
+// sourcePull attempts to pull the image source code based on the manifest list
+func sourcePull(cli command.Cli, namedRef reference.Named, opts PullOptions) error {
+	platform := opts.platform
+	if platform == "" {
+		platform = runtime.GOOS + "/" + runtime.GOARCH
+	}
+
+	manifest, err := getSourceManifest(cli, namedRef, platform)
+	if err != nil {
+		return err
+	}
+
+	repoDigest, err := getSourceRepoDigest(manifest, platform)
+	if err != nil {
+		return err
+	}
+
+	// Pull source image located at repoDigest
+	srcOptions := PullOptions{
+		remote: 	repoDigest,
+		all: 		false,
+		platform: 	opts.platform,
+		untrusted: 	opts.untrusted,
+		source:		false,
+	}
+
+	return RunPull(cli, srcOptions)
+}
+
+func getSourceManifest(cli command.Cli, namedRef reference.Named, platform string) (manifestlist.ManifestDescriptor, error) {
+	ctx := context.Background()
+	registryClient := cli.RegistryClient(false) // TODO: add flag to PullOptions to allow pulling manifest lists from insecure registries
+
+	// Check remote manifest list
+	manifestList, err := registryClient.GetManifestList(ctx, namedRef)
+	if err != nil {
+		return manifestlist.ManifestDescriptor{}, err
+	}
+	
+	targetRepo, err := registry.ParseRepositoryInfo(namedRef)
+	if err != nil {
+		return manifestlist.ManifestDescriptor{}, err
+	}
+
+	// Resolve desired platform
+	clientPlatform, err := platforms.Parse(platform)
+	if err != nil {
+		return manifestlist.ManifestDescriptor{}, err
+	}
+
+	// Check for matching platform in manifest list
+	platformMatch := platforms.NewMatcher(clientPlatform)
+	for _, img := range manifestList {
+		mfd, err := buildManifestDescriptor(targetRepo, img)
+		if err != nil {
+			return manifestlist.ManifestDescriptor{}, errors.Wrap(err, "failed to assemble ManifestDescriptor")
+		}
+
+		if platformMatch.Match(*types.OCIPlatform(&mfd.Platform)) {
+			return mfd, nil
+		}
+	}
+
+	return manifestlist.ManifestDescriptor{}, errors.New("no match for platform " + platform)
+}
+
+func getSourceRepoDigest(descriptor manifestlist.ManifestDescriptor, platform string) (string, error) {
+	// Obtain RepoDigest for source image
+	// TODO: For now it uses the "os-features" field to store the link to source. Eventually this should change
+	for _, entry := range descriptor.Platform.OSFeatures {
+		if strings.HasPrefix(entry, "source:") {
+			return strings.TrimPrefix(entry, "source:"), nil
+		}
+	}
+
+	return "", errors.New("no source image for platform " + platform)
+}
+
+func buildManifestDescriptor(targetRepo *registry.RepositoryInfo, imageManifest types.ImageManifest) (manifestlist.ManifestDescriptor, error) {
+	repoInfo, err := registry.ParseRepositoryInfo(imageManifest.Ref)
+	if err != nil {
+		return manifestlist.ManifestDescriptor{}, err
+	}
+
+	manifestRepoHostname := reference.Domain(repoInfo.Name)
+	targetRepoHostname := reference.Domain(targetRepo.Name)
+	if manifestRepoHostname != targetRepoHostname {
+		return manifestlist.ManifestDescriptor{}, errors.Errorf("cannot use source images from a different registry than the target image: %s != %s", manifestRepoHostname, targetRepoHostname)
+	}
+
+	manifest := manifestlist.ManifestDescriptor{
+		Descriptor: distribution.Descriptor{
+			Digest:    imageManifest.Descriptor.Digest,
+			Size:      imageManifest.Descriptor.Size,
+			MediaType: imageManifest.Descriptor.MediaType,
+		},
+	}
+
+	platform := types.PlatformSpecFromOCI(imageManifest.Descriptor.Platform)
+	if platform != nil {
+		manifest.Platform = *platform
+	}
+
+	if err = manifest.Descriptor.Digest.Validate(); err != nil {
+		return manifestlist.ManifestDescriptor{}, errors.Wrapf(err,
+			"digest parse of image %q failed", imageManifest.Ref)
+	}
+
+	return manifest, nil
 }
